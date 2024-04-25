1635564d76d78b2dd8dc9b48d99a7d746fb3a278
Deepanshi <155594353+21Deepanshi@users.noreply.github.com>
2024-04-24 19:21:23 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -24,7 +24,7 @@ let sections = document.querySelectorAll('section');
 
 window.onscroll = () => {
     sections.forEach(sec => {
-        let top = wimdow.scrollY;
+        let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
